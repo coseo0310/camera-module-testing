c780b894a538aedb07468503bcf8cbcc490dda6e
coseo0310 <co.seo@lomin.ai>
2022-05-30 15:44:48 +0900

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,13 @@
 // import tslint from "rollup-plugin-tslint";
 import { uglify } from "rollup-plugin-uglify";
 // typescript({ clean: true, sourceMap: false }), tslint(),
+
+const isWorker = true;
+
 export default {
-  input: "./module/detector.js",
+  input: isWorker ? "./src/worker.js" : "./module/detector.js",
   output: {
-    file: "example/module/detector.js",
+    file: isWorker ? "./src/worker.js" : "./example/module/detector.js",
     format: "esm",
   },
   plugins: [uglify()],
